d8e791a9b29c5824046d0b3b9280d1366b208f5b
Harrison Lavins <hlavins313@gmail.com>
2023-11-27 22:28:55 -0500

    Arduino: Test motor controller

---
+++ arduino/platformController/platformController.ino
@@ -0,0 +1,55 @@
+// connect motor controller pins to Arduino digital pins
+// motor one
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+// motor two
+int enB = 5;
+int in3 = 7;
+int in4 = 6;
+
+void setup() {
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+void demoTwo(){
+  // this function will run the motors across the range of possible speeds
+  // Note: maximum speed is determined by the motor itself and the operating voltage
+  // the PWM values sent my the analogWrite() are fractions of the maximum speed possible by hardware
+
+  // turn on motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  // accelerate from zero to maximum speed
+  for (int i = 0; i < 256; i++)
+  {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+  // decelerate from maximum speed to zero
+  for (int i = 255; i >= 0; --i)
+  {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+  // turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void loop() {
+  // test motors through their speed range
+  demoTwo();
+}
