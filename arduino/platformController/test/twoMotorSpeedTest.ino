bd8e1028b3cfa55eb032b1a55b29e4890bad7fc6
Harrison Lavins <hlavins313@gmail.com>
2023-12-01 21:56:46 -0500

    WIP: 2 motor speed and direction test

---
+++ arduino/platformController/test/twoMotorSpeedTest.ino
@@ -0,0 +1,28 @@
+// connect motor controller pins to Arduino digital pins
+// motor one
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+// motor two
+int enB = 5;
+int in3 = 7;
+int in4 = 6;
+
+void setup() {
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+void motorSpeedTest(){
+  // 
+ 
+}
+
+void loop() {
+  // test motor speed in both directions
+}
