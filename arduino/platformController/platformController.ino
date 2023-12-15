6b0a6767e1b911c374757d0caeac6de963696684
Harrison Lavins <hlavins313@gmail.com>
2023-12-15 02:42:32 -0500

    WIP: add front motors

--- arduino/platformController/platformController.ino
+++ arduino/platformController/platformController.ino
@@ -8,14 +8,32 @@ int enB = 10; // PWM pin
 int RR_neg = 9;
 int RR_pos = 8;
 
+// motor three - Right Front
+int enC = 6;  // PWM pin
+int RF_pos = 7;
+int RF_neg = 5;
+
+// motor four - Left Front
+int enD = 3; // PWM pin
+int LF_pos = 2; 
+int LF_neg = 4;
+
+
 void setup() {
   // set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
+  pinMode(enC, OUTPUT);
+  pinMode(enD, OUTPUT);
+
   pinMode(LR_pos, OUTPUT);
   pinMode(LR_neg, OUTPUT);
   pinMode(RR_neg, OUTPUT);
   pinMode(RR_pos, OUTPUT);
+  pinMode(LF_pos, OUTPUT);
+  pinMode(LF_neg, OUTPUT);
+  pinMode(RF_neg, OUTPUT);
+  pinMode(RF_pos, OUTPUT);
 }
 
 void forward() {
@@ -28,13 +46,28 @@ void forward() {
   digitalWrite(RR_neg, LOW);
   digitalWrite(RR_pos, HIGH);
   analogWrite(enB, 200);   // set speed to 200 out of possible range 0-255
+
+  //turn on motor C
+  digitalWrite(LF_pos, HIGH);
+  digitalWrite(LF_neg, LOW);
+  analogWrite(enC, 200);   //set speed to 200 out of possible range 0-255
+
+  //turn on motor D
+  digitalWrite(RF_neg, LOW);
+  digitalWrite(RF_pos, HIGH);
+  analogWrite(enD, 200);   // set speed to 200 out of possible range 0-255
 }
 
 void reverse() {
- digitalWrite(LR_pos, LOW);
+  digitalWrite(LR_pos, LOW);
   digitalWrite(LR_neg, HIGH);
   digitalWrite(RR_neg, HIGH);
   digitalWrite(RR_pos, LOW);
+
+  digitalWrite(LF_pos, LOW);
+  digitalWrite(LF_neg, HIGH);
+  digitalWrite(RF_neg, HIGH);
+  digitalWrite(RF_pos, LOW);
 }
 
 void run() 
@@ -48,11 +81,15 @@ void run()
   delay(2000);
 
 //now turn off motors
-  digitalWrite(LR_pos, LOW);
+  digitalWrite(LR_pos, LOW); //rear
   digitalWrite(LR_neg, LOW);
   digitalWrite(RR_neg, LOW);
   digitalWrite(RR_pos, LOW);
 
+  digitalWrite(LF_pos, LOW); //front
+  digitalWrite(LF_neg, LOW);
+  digitalWrite(RF_neg, LOW);
+  digitalWrite(RF_pos, LOW);
 }
 
 
