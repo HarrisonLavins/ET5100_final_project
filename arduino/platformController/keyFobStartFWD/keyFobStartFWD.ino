10d8a850ccbc18becab3c6da9b423d6f6288ec96
Harrison Lavins <hlavins313@gmail.com>
2023-12-18 20:38:57 -0500

    Key-start platform controller with front-wheel drive

--- arduino/platformController/keyFobStartFWD/keyFobStartFWD.ino
+++ arduino/platformController/keyFobStartFWD/keyFobStartFWD.ino
@@ -8,6 +8,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+// ------- Setup for Key Reader ----------
+
 #define SS_PIN 10
 #define RST_PIN 9
 
@@ -18,9 +20,35 @@ String tagID = "";
 // Create class instances
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+
+// ------- Setup for Motor Drivers (Front-Wheel Drive only) ----------
+// connect motor controller pins to Arduino digital pins
+// motor one - Left Front
+int enA = 3; // PWM pin
+int LF_pos = 2;
+int LF_neg = 4;
+// motor two - Right Front
+int enB = 6; // PWM pin
+int RF_neg = 7;
+int RF_pos = 5;
+
+
+
 // setup() function
 void setup() 
 {
+ // Setup for motor drivers
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+
+  pinMode(LF_pos, OUTPUT);
+  pinMode(LF_neg, OUTPUT);
+  pinMode(RF_neg, OUTPUT);
+  pinMode(RF_pos, OUTPUT);
+
+
+  // Setup for key reader
   Serial.begin(9600);
   SPI.begin();
 
@@ -31,6 +59,42 @@ void setup()
   
   Serial.println("--------------------------");
   Serial.println(" Waiting for a Key... ");
+}
+
+void forward() {
+  //turn on Left Front motor
+  digitalWrite(LF_pos, HIGH);
+  digitalWrite(LF_neg, LOW);
+  analogWrite(enA, 200);   //set speed to 200 out of possible range 0-255
+
+  //turn on Right Front motor
+  digitalWrite(RF_neg, HIGH);
+  digitalWrite(RF_pos, LOW);
+  analogWrite(enB, 200);   // set speed to 200 out of possible range 0-255
+}
+
+void reverse() {
+  digitalWrite(LF_pos, LOW);
+  digitalWrite(LF_neg, HIGH);
+  digitalWrite(RF_neg, LOW);
+  digitalWrite(RF_pos, HIGH);
+}
+
+void run() 
+{
+  //this function will run the motors in both directions at a fixed speed
+  forward();
+  delay(2000);
+
+  //now change motor directions
+  reverse();
+  delay(2000);
+
+//now turn off motors
+  digitalWrite(LF_pos, LOW); //front
+  digitalWrite(LF_neg, LOW);
+  digitalWrite(RF_neg, LOW);
+  digitalWrite(RF_pos, LOW);
 }
 
 
@@ -44,7 +108,9 @@ void loop()
     if (tagID == MasterTag){
       Serial.println(" Starting Car!");
       Serial.println("--------------------------");
-       // TODO: Put the code for turning on the motors here
+       // Start the car
+       run();
+      Serial.println(" Running! ");
     }
 
     else{
